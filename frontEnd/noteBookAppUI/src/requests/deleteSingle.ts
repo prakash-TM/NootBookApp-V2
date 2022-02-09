fb3dbced15ee27b6307ab8077850c74983eff7bc
Prakash Muthusamy <prakash.muthusamy@cesltd.com>
2022-02-09 12:00:17 +0530

    added aws ip address

--- frontEnd/noteBookAppUI/src/requests/deleteSingle.ts
+++ frontEnd/noteBookAppUI/src/requests/deleteSingle.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const DeleteSingleReq=async (deleteWord:any,deleteSingleApi:any)=>{
     const responseData=await axios
-    .delete(`http://ec2-3-108-236-18.ap-south-1.compute.amazonaws.com:8000/${deleteSingleApi}`, { params: { title: deleteWord }})
+    .delete(`http://ec2-52-66-244-234.ap-south-1.compute.amazonaws.com:8000/${deleteSingleApi}`, { params: { title: deleteWord }})
     return(responseData)
 }
 export default DeleteSingleReq
--- frontEnd/noteBookAppUI/src/requests/getAll.ts
+++ frontEnd/noteBookAppUI/src/requests/getAll.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const GetAllReq=async (getAllApi:any)=>{
     const responseData=await axios
-    .get(`http://ec2-3-108-236-18.ap-south-1.compute.amazonaws.com:8000/${getAllApi}`)
+    .get(`http://ec2-52-66-244-234.ap-south-1.compute.amazonaws.com:8000/${getAllApi}`)
     return(responseData)
 }
 export default GetAllReq
