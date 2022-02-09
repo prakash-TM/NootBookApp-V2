746793d2e67b268b5f9daf7fc42f5a45bb736851
Prakash Muthusamy <prakash.muthusamy@cesltd.com>
2022-02-09 13:27:38 +0530

    added shell commends for front and back end

---
+++ deploy_be.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+echo "git pull started"
+git pull
+echo "git pull completed"
+
+echo "install nodemodules start"
+npm install
+echo "installation completed"
+
+echo "build started"
+npm run build
+echo "build completed"
+
+echo "pm2 started"
+pm2 start build/index.js
+echo "pm2 running"
+
+echo "pm2 saving"
+pm2 save
+echo "pm2 saved"
