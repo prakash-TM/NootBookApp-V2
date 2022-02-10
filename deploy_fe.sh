5199ce1d7f2b5e9b56c5115cc399f9ae568a6d4c
Prakash Muthusamy <prakash.muthusamy@cesltd.com>
2022-02-10 10:14:22 +0530

    added shell scripts

--- deploy_fe.sh
+++ deploy_fe.sh
@@ -11,10 +11,9 @@ echo "installation completed"
 echo "build started"
 npm run build
 echo "build completed"
-cd ..
-cd ..
+
 echo "pm2 started"
-pm2 start frontEnd/noteBookAppUI/frontend.js -f
+pm2 start frontend.js -f
 echo "pm2 running"
 
 echo "pm2 saving"
