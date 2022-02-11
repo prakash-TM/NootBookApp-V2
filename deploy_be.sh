94e0328f964934244b772da773f010faeadff996
Prakash Muthusamy <prakash.muthusamy@cesltd.com>
2022-02-11 13:35:54 +0530

    added pm2 backend json

--- deploy_be.sh
+++ deploy_be.sh
@@ -14,7 +14,8 @@ npm run build
 echo "build completed"
 
 echo "pm2 started"
-pm2 start build/backend.js -f
+# pm2 start build/backend.js -f
+pm2 start pm2B.json
 echo "pm2 running"
 
 echo "pm2 saving"
